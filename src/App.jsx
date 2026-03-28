2a55711c905413b6b968048f75ef3847faf45005
subarnakatwal123 <subwrn@gmail.com>
2026-03-28 18:50:21 +0545

    Remove blog/admin, add vault import/export - Blog and admin dashboard removed entirely - Import passwords from Chrome/Firefox/Edge CSV or JSON - Export vault as Chrome-compatible CSV - Import result shows count imported/skipped

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import { AuthProvider } from './context/AuthContext'
 import { CryptoProvider } from './context/CryptoContext'
 import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
-import AdminRoute from './components/AdminRoute'
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Blog from './pages/Blog'
 import Vault from './pages/Vault'
 import ForgotPassword from './pages/ForgotPassword'
 import ResetPassword from './pages/ResetPassword'
 import ChangeMasterPassword from './pages/ChangeMasterPassword'
-import AdminIndex from './pages/admin/AdminIndex'
-import PostEditor from './pages/admin/PostEditor'
 
 export default function App() {
   return (
@@ -25,7 +21,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
@@ -44,30 +39,6 @@ export default function App() {
               <ProtectedRoute>
                 <ChangeMasterPassword />
               </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <AdminRoute>
-                <AdminIndex />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/post/new"
-            element={
-              <AdminRoute>
-                <PostEditor />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/post/:id"
-            element={
-              <AdminRoute>
-                <PostEditor />
-              </AdminRoute>
             }
           />
         </Routes>
